d766968b06ba98a55bf99e1a8b97828a62c8e8cf
anticdope <pruettdillan@gmail.com>
2025-01-16 11:51:27 -0800

    Update Kong_Motor_Drive.ino

--- Kong_Motor_Drive/Kong_Motor_Drive.ino
+++ Kong_Motor_Drive/Kong_Motor_Drive.ino
@@ -5,19 +5,32 @@ DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
 
 #define DRIVER_PUL 2  // Pulse pin
 #define DRIVER_DIR 5  // Direction pin
+#define ENABLE_PIN 10
 
 void setup() {
   dmx_slave.enable();
   pinMode(DRIVER_PUL, OUTPUT);
   pinMode(DRIVER_DIR, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
   digitalWrite(DRIVER_DIR, LOW);  // Set direction to low initially
+  digitalWrite(ENABLE_PIN, HIGH);  // Set enable high initially (triggers LOW)
 }
 
 void loop() {
-  int dmxValue = dmx_slave.getChannelValue(1);
-  int speed = map(dmxValue, 0, 255, 2000, 400);
+  int speeddmxValue = dmx_slave.getChannelValue(1);
+  int enabledmxValue = dmx_slave.getChannelValue(2);
+  
+  if (enabledmxValue == 255) {
+    digitalWrite(ENABLE_PIN, LOW);
+  }
+  else {
+    digitalWrite(ENABLE_PIN, HIGH);
+  }
+
+  int speed = map(speeddmxValue, 0, 255, 2000, 400);
   digitalWrite(DRIVER_PUL, HIGH);
   delayMicroseconds(speed);
   digitalWrite(DRIVER_PUL, LOW);
   delayMicroseconds(speed);
+
 }
